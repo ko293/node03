20b2f72fcfc9af40736613117c49a9cc30929ef7
surapong ko <surapong293@gmail.com>
2019-09-02 15:04:24 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    const url = req.url
+    //console.log(url)
+    switch (url){
+        case '/':
+            res.end('<h1>Home Page</h1>')
+            break;
+        case '/about':
+            res.end('<h1>About Page</h1>')
+            break;
+        default:
+            res.end('<h1>Page Not Found</h1>')
+            break;
+    }
+    //res.end('<h1>Hello Ko</h1>')
+})
+
+//server.listen(3000)
+const port = process.env.PORT || 3000
+server.listen(port)
